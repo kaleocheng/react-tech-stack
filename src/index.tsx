0f04c7ecd04fe73252c6929734b9f126f7f65fc4
Kaleo <kaleocheng@gmail.com>
2022-04-05 21:00:37 +0800

    Fix the stack item style

--- src/index.tsx
+++ src/index.tsx
@@ -18,9 +18,9 @@ const renderStackItemCard = (item: Item) => {
         logo = <div className="w-8 h-8"><img src={item.logo}></img></div>
     }
     return (
-        <div className="flex flex-wrap space-x-2 > * + *">
+        <div className="md:flex md:flex-wrap grid place-items-center">
             {logo}
-            <div className="self-center text-neutral-800">{item.name}</div>
+            <div className="text-neutral-800 px-1">{item.name}</div>
         </div>
     )
 }
